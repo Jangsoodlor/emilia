29d1d36b255c7fcf0fbbcfe04fabce7ae0e86abe
Jangsoodlor <celica2548@gmail.com>
2022-11-06 11:55:46 +0700

    add GUI file selector
    uraaaaaaaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from rembg import remove
 from PIL import Image as im
 from datetime import datetime
 from tkinter import *
+from tkinter import filedialog
 
 def getDateTimeStr():
     now = datetime.now()
     return now.strftime('%Y%m%d_%H%M%S')
 
+def openfile():
+    file_path = filedialog.askopenfile(filetypes=[('PNG Files', '.png'), ('JPEG Files', '.jpg')])
+    return file_path.name
+
 def bg_del():
-    input_path = str(input('insert your picture directory baka: '))
+    # str(input('insert your picture directory baka: '))
+    input_path = openfile()
     img_dir = rf'{input_path}'
     output_path = 'testfile/' + getDateTimeStr() + '.png'
     img_open = im.open(img_dir)
